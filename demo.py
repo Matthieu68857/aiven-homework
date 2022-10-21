4cfe66a9f759604b805cd6a07adc11f63e72aebc
Matthieu CORNILLON <matthieu.cornillon@adeo.com>
2022-10-21 09:14:14 +0200

    First push

--- demo.py
+++ demo.py
@@ -5,11 +5,12 @@ from datetime import datetime
 import json
 import uuid
 
+
 class KafkaDAO(object):
-    
+
     AIVEN_KAFKA_HOST = "kafka-homework-cornillon-a592.aivencloud.com"
     AIVEN_KAFKA_SSL_PORT = 26618
-    
+
     def __init__(self):
         print("[Initiating Kafka client]")
         self.producer = KafkaProducer(
@@ -18,11 +19,11 @@ class KafkaDAO(object):
             ssl_cafile="ca.pem",
             ssl_certfile="service.cert",
             ssl_keyfile="service.key",
-            value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
             acks="all",
         )
-        
-    def send(self, topic:str, event:Dict):
+
+    def send(self, topic: str, event: Dict):
         print(f"[Publishing one message to topic {topic}...]")
         result = self.producer.send(
             topic=topic,
@@ -30,44 +31,41 @@ class KafkaDAO(object):
         )
         print(result.get())
 
+
 def main():
-    
+
     order1 = {
-        "id" : str(uuid.uuid4()),
-        "date" : datetime.now().isoformat(),
-        "items" : [
+        "id": str(uuid.uuid4()),
+        "date": datetime.now().isoformat(),
+        "items": [
             {
-                "id" : "ref#314698",
-                "name" : "Screwdriver",
-                "quantity" : 1,
-                "price_euro" : 12
+                "id": "ref#314698",
+                "name": "Screwdriver",
+                "quantity": 1,
+                "price_euro": 12,
             },
-            {
-                "id" : "ref#481516",
-                "name" : "Hammer",
-                "quantity" : 2,
-                "price_euro" : 9
-            }
-        ]
+            {"id": "ref#481516", "name": "Hammer", "quantity": 2, "price_euro": 9},
+        ],
     }
-    
+
     order2 = {
-        "id" : str(uuid.uuid4()),
-        "date" : datetime.now().isoformat(),
-        "items" : [
+        "id": str(uuid.uuid4()),
+        "date": datetime.now().isoformat(),
+        "items": [
             {
-                "id" : "ref#314698",
-                "name" : "Nails 25mm",
-                "quantity" : 50,
-                "price_euro" : 0.06
+                "id": "ref#314698",
+                "name": "Nails 25mm",
+                "quantity": 50,
+                "price_euro": 0.06,
             }
-        ]
+        ],
     }
-    
+
     kafka = KafkaDAO()
-    
+
     kafka.send("orders-topic", order1)
     kafka.send("orders-topic", order2)
+
 
 if __name__ == "__main__":
     main()
